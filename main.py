3e8fcfb53743d26501c4c6afe2cf8d771623c9e1
ChrisLoucaides <christosloucaides@gmail.com>
2026-03-29 09:16:10 +0100

    run bot in main thread

--- main.py
+++ main.py
@@ -55,15 +55,30 @@ def home():
     return "Bot is running"
 
 
-def run_bot():
-    logger.debug("Starting Discord bot thread...")
-    logger.debug(f"TOKEN EXISTS:  {bool(DISCORD_TOKEN)}")
+def run_flask():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+
+# Start Flask in background thread
+Thread(target=run_flask).start()
+
+# IMPORTANT: bot runs in MAIN thread
+logger.info("Starting Discord bot...")
+try:
     bot.run(DISCORD_TOKEN)
+except Exception as e:
+    print("BOT ERROR:", e)
 
-
-# Start Discord bot in a separate thread
-Thread(target=run_bot).start()
-
-# Run Flask in main thread (Render health checks)
-port = int(os.environ.get("PORT", 8080))
-app.run(host="0.0.0.0", port=port)
+# def run_bot():
+#     logger.debug("Starting Discord bot thread...")
+#     logger.debug(f"TOKEN EXISTS:  {bool(DISCORD_TOKEN)}")
+#     bot.run(DISCORD_TOKEN)
+#
+#
+# # Start Discord bot in a separate thread
+# Thread(target=run_bot).start()
+#
+# # Run Flask in main thread (Render health checks)
+# port = int(os.environ.get("PORT", 8080))
+# app.run(host="0.0.0.0", port=port)
